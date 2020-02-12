4943a9448ff6f0275f2f84bb341911197d00d9af
Rio Sanka <threehouse.second@gmail.com>
2020-02-12 21:33:03 +0900

    コード修正

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,7 +3,27 @@ import '../index.css';
 
 import Board from './Board';
 
+function calculateWinner(squares: any[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
+// stateをthis.stateでまとめたい
 export default class Game extends React.Component {
   state = {
     history: [{
@@ -17,6 +37,7 @@ export default class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -54,7 +75,7 @@ export default class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = 'Winner' + winner;
+      status = `Winner${winner}`;
     } else {
       status = `Next player:${this.state.XIsNext ? 'X' : 'O'}`;
     }
@@ -74,24 +95,4 @@ export default class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares: any[]) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
